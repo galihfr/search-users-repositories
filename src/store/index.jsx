6723c3e1240cef48510db4e7ad3135afa0ae7a77
galihfr <galihfathulrohmi@gmail.com>
2022-02-09 15:18:49 +0700

    fixing store redux

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -2,17 +2,6 @@ import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 
-// var getComposeEnhancers = () => {
-//     if (window.navigator.userAgent.includes('Chrome')) {
-//       return compose(
-//         applyMiddleware(thunk)
-//         ,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-//       );
-//     }
-//     return compose(applyMiddleware(thunk) );
-// };
-
-// var Store = createStore(reducers, getComposeEnhancers() );
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const Store = createStore(
   reducers,
