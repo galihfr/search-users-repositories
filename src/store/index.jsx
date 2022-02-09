50d1b71aa6b635603792f2d5551bb5d6bc121405
galihfr <galihfathulrohmi@gmail.com>
2022-02-09 15:09:43 +0700

    adjust store

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -2,16 +2,21 @@ import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers';
 
-var getComposeEnhancers = () => {
-    if (window.navigator.userAgent.includes('Chrome')) {
-      return compose(
-        applyMiddleware(thunk)
-        ,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-      );
-    }
-    return compose(applyMiddleware(thunk) );
-};
+// var getComposeEnhancers = () => {
+//     if (window.navigator.userAgent.includes('Chrome')) {
+//       return compose(
+//         applyMiddleware(thunk)
+//         ,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//       );
+//     }
+//     return compose(applyMiddleware(thunk) );
+// };
 
-var Store = createStore(reducers, getComposeEnhancers() );
+// var Store = createStore(reducers, getComposeEnhancers() );
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const Store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 export default Store;
